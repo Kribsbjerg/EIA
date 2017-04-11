0465bee2271f1e97e89bab2b4249d23c45175871
Kris <Kris@KRIS-PC>
2017-04-11 10:55:39 +0200

    Comments and regions for ContactInformation and ContactHours

--- ClassesForProjectEIA/ClassesForProjectEIA/ContactHours.cs
+++ ClassesForProjectEIA/ClassesForProjectEIA/ContactHours.cs
@@ -8,13 +8,31 @@ namespace ClassesForProjectEIA
 {
     class ContactHours
     {
-        DateTime From { get; set; }
-        DateTime To { get; set; }
+        #region Properties
 
+        /// <summary>
+        /// Holds the time for the first possible contact hour 
+        /// </summary>
+        DateTime From { get; set; }
+
+        /// <summary>
+        /// Holds the time for the last possible contact hour
+        /// </summary>
+        DateTime To { get; set; }
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
         public ContactHours(DateTime from, DateTime to)
         {
             From = from;
             To = to;
         }
+        #endregion
     }
 }
