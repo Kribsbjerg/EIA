cfb9d5347ac38315a3c5f25ae1a8749b53332f70
Kris <Kris@KRIS-PC>
2017-04-18 10:48:28 +0200

    .

---
+++ EIAUI/EIAUI/ViewModel/Visitation/VisitationCardListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace EIAUI.ViewModel.Visitation
+{
+    class VisitationCardListViewModel : BaseViewModel
+    {
+        public List<VisitationCardViewModel> Items { get; set; }
+    }
+}
